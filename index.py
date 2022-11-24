af3dce8f2d11744d69ee25348cf8177cd40480f6
gonzaloiniesta <60652830+gonzaloiniesta@users.noreply.github.com>
2022-11-24 22:33:47 +0100

    Create index.py

---
+++ index.py
@@ -0,0 +1,11 @@
+print("Fibonacci sequence upto 10:")
+# generate fibonacci sequence
+n1, n2 = 0, 1
+count = 0
+
+while count < 10:
+  print(n1)
+  nth = n1 + n2
+  n1 = n2
+  n2 = nth
+  count += 1
